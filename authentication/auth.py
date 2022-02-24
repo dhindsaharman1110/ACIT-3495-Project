3cc758c51c628bd4e7e8656d7a63a499c4b39322
Sobakiin <wgreen16@my.bcit.ca>
2022-02-23 17:36:47 -0800

    turned the flask app into a python app.

--- authentication/auth.py
+++ authentication/auth.py
@@ -1,16 +1,40 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
-from . import db
-from .models import User
-from flask_login import login_user, login_required, logout_user
-auth = Blueprint('auth', __name__)
+from flask import render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, login_required, logout_user, current_user, LoginManager
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# auth = Blueprint('auth', __name__)
+
+app = Flask(__name__)
+db = SQLAlchemy()
 
 
+app = Flask(__name__)
 
-@auth.route('/login')
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+db.init_app(app)
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
+
+@app.route('/')
 def login():
     return render_template('login.html')
 
-@auth.route('/login', methods=['POST'])
+@app.route('/', methods=['POST'])
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
@@ -20,16 +44,16 @@ def login_post():
 
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again')
-        return redirect(url_for('auth.login'))
+        return redirect('/')
     
     login_user(user, remember=remember)
-    return redirect(url_for('main.profile'))
+    return redirect('/profile')
 
-@auth.route('/signup')
+@app.route('/signup')
 def signup():
-    return 'Signup'
+    return render_template('signup.html')
 
-@auth.route('/signup', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def signup_post():
     # code to validate and add user to database goes here
     email = request.form.get('email')
@@ -39,7 +63,7 @@ def signup_post():
     user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
-        return redirect(url_for('auth.signup'))
+        return redirect('/signup')
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
@@ -49,16 +73,20 @@ def signup_post():
     db.session.commit()
     if user: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
-    return redirect(url_for('auth.login'))
+        return redirect('/signup')
+    return redirect('/')
 
-
-
-@auth.route('/logout')
+@app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect('/')
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
 
 if __name__ == "__main__":
-    auth.run(port=8100)
+    app.run(port=8100)
+
